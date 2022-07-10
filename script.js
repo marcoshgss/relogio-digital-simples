0aa28b1a34ee712bd0e0308a2b16d60c01be3b95
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-07-10 20:34:46 -0300

    Finalizei tudo

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function clock() {
-    var h = new Date().getHours();
-    var m = new Date().getMinutes();
-    var s = new Date().getSeconds();
+const horas = document.getElementById('hora');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
-    var hours   = document.getElementById("horas").innerHTML = h;
-    var minutes = document.getElementById("minutos").innerHTML = m;
-    var segunds = document.getElementById("segundos").innerHTML = s;
+const relogio = setInterval(function time(){
+    let data = new Date();
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let sec = data.getSeconds();
 
-}
+    if(hr<10){
+        hr = '0'+hr;
+    }
 
-var interval = setInterval(clock, 1000);
+    if(min<10){
+        min = '0'+min;
+    }
+
+    if(sec<10){
+        sec = '0'+sec;
+    }
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;    
+
+})
